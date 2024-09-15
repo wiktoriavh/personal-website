f46cacd59b4b35494ae17782a591d600b46a1063
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2024-09-15 17:11:37 +0200

    feat: create root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,29 @@
-import Link from "next/link";
+import { LinkButton } from "~/components/Button";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+    <>
+      <h1 className="font-baloo text-8xl font-bold text-wi-brown-500">
+        Moin, I'm Wiktoria
+      </h1>
+      <p className="max-w-[70ch] py-6 text-2xl">
+        I'm a frontend software engineer at{" "}
+        <span className="font-bold">Trusted Shops</span>, where I build websites
+        for B2C. My tech stack includes React with Next.js, and Tailwind CSS for
+        styling. With a strong interest in accessibility, I pay extra attention
+        to how I build web applications to ensure they are usable by everyone.
+        I'm also passionate about continuous learning—currently, I'm reading{" "}
+        <span className="font-bold italic">
+          A Philosophy Of Software Design
+        </span>{" "}
+        to expand my knowledge.
+      </p>
+      <div className="flex gap-6">
+        <LinkButton href="https://github.com/wiktoriavh">GitHub</LinkButton>
+        <LinkButton href="https://www.linkedin.com/in/wiktoria-vh/">
+          LinkedIn
+        </LinkButton>
       </div>
-    </main>
+    </>
   );
 }

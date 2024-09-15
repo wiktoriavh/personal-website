19e47b88538e388117e4090e103a2d614fc2b523
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2024-09-15 17:12:35 +0200

    feat: configure variable font and blob shape

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,32 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+const brandColors = {
+  wi: {
+    brown: {
+      500: "#4D342F",
+    },
+    red: {
+      50: "#FFE9E3",
+      100: "#FADBD3",
+      600: "#CC3816",
+    },
+    black: {
+      50: "#F1F1F1",
+    },
+  },
+};
+
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        sans: [...fontFamily.sans],
+        baloo: ["Baloo", "Times New Roman", "serif"],
+      },
+      colors: {
+        ...brandColors,
       },
     },
   },

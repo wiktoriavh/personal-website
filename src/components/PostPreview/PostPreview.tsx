7087b45bb7ef86108a292c3cefc0dfabbf19d969
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2024-11-13 19:10:43 +0100

    refactor: remove fixed height and width

--- src/components/PostPreview/PostPreview.tsx
+++ src/components/PostPreview/PostPreview.tsx
@@ -14,7 +14,7 @@ export const PostPreview: FC<PostPreviewProps> = ({ src, title, blurb }) => {
       style={{ gridTemplateColumns: "1fr 3fr" }}
     >
       <BlobThumbnail src={src} title={title} />
-      <div className="grid h-36 w-[70ch] grid-rows-5 gap-4">
+      <div className="grid grid-rows-5 gap-4">
         <h2 className="row-span-2 font-baloo text-2xl font-bold">{title}</h2>
         <p className="row-span-3 line-clamp-3 self-start text-base">{blurb}</p>
       </div>

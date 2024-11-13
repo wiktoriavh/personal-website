6d648a72e725ab16a908bec3a4e60c7f14b14a10
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2024-11-13 19:10:07 +0100

    feat: allow class name for thumbnail

--- src/components/BlobThumbnail/BlobThumbnail.tsx
+++ src/components/BlobThumbnail/BlobThumbnail.tsx
@@ -4,12 +4,16 @@ import { FC } from "react";
 type Props = {
   src: string;
   title: string;
+  className?: string;
 };
 
-export const BlobThumbnail: FC<Props> = ({ src, title }) => {
+export const BlobThumbnail: FC<Props> = ({ src, title, className }) => {
   return (
     <Image
-      className="form-blob-2 aspect-video w-full object-cover"
+      className={[
+        "form-blob-2 aspect-video w-full object-cover",
+        className ?? "",
+      ].join(" ")}
       src={src}
       alt={title}
       width={160}

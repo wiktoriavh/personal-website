6a37294a26fb3a85fc357a66f225b282b5cc9ec1
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2024-11-16 19:15:08 +0100

    feat: create component for hidden svg

---
+++ src/components/HiddenSvgBlobs/HiddenSvgBlobs.tsx
@@ -0,0 +1,9 @@
+export const HiddenSvgBlobs = () => {
+  return (
+    <svg width="0" height="0">
+      <clipPath id="blob" clipPathUnits="objectBoundingBox">
+        <path d="M0.05 0.1C0.127 0.043 0.304 0.017 0.483 0.017C0.662 0.017 0.844 0.038 0.935 0.107C1.027 0.176 1.026 0.432 0.933 0.55C0.838 0.668 0.656 0.661 0.49 0.652C0.324 0.643 0.163 0.643 0.077 0.522C-0.01 0.399 -0.029 0.175 0.05 0.1Z" />
+      </clipPath>
+    </svg>
+  );
+};
